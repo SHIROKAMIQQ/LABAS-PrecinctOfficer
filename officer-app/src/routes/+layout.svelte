c704d1d0cf26a63c0424c8d33b225a641ebee9c8
lian-nicoli <liannicoli27@gmail.com>
2026-04-25 21:31:17 +0800

    feat: frontend to display voter info

---
+++ officer-app/src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { page } from "$app/state";
+  	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
+
+	let activeUrl = $derived(page.url.pathname);	
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<div id="main" class="flex items-center justify-center min-h-screen">
+	<Navbar class="absolute top-0 left-0">
+		<NavBrand href="/">
+			<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">L.A.B.A.S.</span>
+		</NavBrand>
+		<NavHamburger />
+		<NavUl {activeUrl}>
+			<NavLi href="/" class="text-xl">Dashboard</NavLi>
+		</NavUl>
+	</Navbar>
+	
+	<div class="mx-auto mt-10 flex w-full max-w-300 flex-col p-10">
+		{@render children()}
+		
+	</div>
+</div>
---
+++ officer-app/src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import { Button } from "flowbite-svelte";
+
+    let name = $state("Full Name");
+    let address = $state("Address");
+    let image = $state("");
+
+    function alertBox(){
+        alert("Your pressed the button!")
+    }
+</script>
+
+<div id="container" class="flex flex-col border w-250 h-150 p-10">
+    <div class="flex flex-row">
+        <!-- display voter image -->
+        {#if image === ""}
+            <div class="flex w-100 h-100 bg-gray-300 rounded-sm items-center justify-center m-2">
+                <span>Display Image</span>
+            </div>
+        {:else}
+            <img src={image} alt="Preview" style="max-width: 400px; max-height: 400px" />
+        {/if}
+
+        <!-- display voter information -->
+        <div class="flex flex-col m-2 mt-6 p-4 gap-2">
+            <h1 class="text-xl text-bold">Full Name: {name}</h1>
+            <h1 class="text-xl text-bold">Location: {address}</h1>
+        </div>
+    </div>
+
+    <!-- generate button -->
+    <div class="flex items-center justify-center p-4 my-auto">
+        <Button color="light" onclick={alertBox}>Generate and Print Ballot</Button>
+    </div>
+</div>
