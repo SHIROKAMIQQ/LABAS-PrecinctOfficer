52d6c49793dc2984504299a5f81aa21304a361fa
mikelReinanArcardo <mikeljuly28@gmail.com>
2026-05-03 00:16:09 +0800

    chore: run formatter

--- officer-app/src/lib/ballot_data.ts
+++ officer-app/src/lib/ballot_data.ts
@@ -1,16 +1,20 @@
 // for fetching ballot data
-export async function getData(fetchFn: typeof fetch, province: string, city: string) {
-  const response = await fetchFn(
-    `http://165.245.190.93:8000/ballot?province=${province}&city=${city}`
-  );
+export async function getData(
+    fetchFn: typeof fetch,
+    province: string,
+    city: string,
+) {
+    const response = await fetchFn(
+        `http://165.245.190.93:8000/ballot?province=${province}&city=${city}`,
+    );
 
-  return response.json();
+    return response.json();
 }
 
 // for Tally Component
 export function getVotes(input: { name: string; vote: number }[]) {
-  return input.map(item => ({
-    "name": item.name,
-    "vote": item.vote
-  }));
+    return input.map((item) => ({
+        name: item.name,
+        vote: item.vote,
+    }));
 }
