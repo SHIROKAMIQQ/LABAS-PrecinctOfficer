d3b0a9e05abb77ea59200035c829c23f2e13311b
mikelReinanArcardo <mikeljuly28@gmail.com>
2026-05-03 00:15:42 +0800

    chore: run formatter

--- officer-app/README.md
+++ officer-app/README.md
@@ -1,19 +1,25 @@
 # LABAS Precinct Officer
 
 ## Prerequisites
+
 Copy what's in the `.env.sample` to your local `.env` file via the ff command:
+
 ```sh
 cp .env.sample .env
 ```
 
 ## Developing
+
 Run locally:
+
 ```sh
 pnpm run dev
 ```
 
 ## Building
+
 To create a production version of your app:
+
 ```sh
 pnpm run build
 ```
--- officer-app/src/routes/tally/+page.server.ts
+++ officer-app/src/routes/tally/+page.server.ts
@@ -2,6 +2,6 @@ import { getData } from "$lib/ballot_data";
 import type { PageServerLoad } from "./national/$types";
 
 export const load: PageServerLoad = async ({ fetch }) => {
-  const data = await getData(fetch, "Metro%Manila", "City%of%Manila");
-  return { data };
-}
+    const data = await getData(fetch, "Metro%Manila", "City%of%Manila");
+    return { data };
+};
