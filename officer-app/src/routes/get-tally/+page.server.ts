1ad5e74d9b5075564207b080c96bd6f7a4a18456
Sidney Lance Fernando <scfernando@up.edu.ph>
2026-05-04 14:29:30 +0800

    refactor: move `getTally` from `ballot_data.ts` to `+page.server.ts`

--- officer-app/src/routes/get-tally/+page.server.ts
+++ officer-app/src/routes/get-tally/+page.server.ts
@@ -1,4 +1,5 @@
-import { getTally } from '$lib/ballot_data';
+import { parse } from 'valibot';
+import { GetTallyResultSchema } from '$lib/types';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch, url }) => {
@@ -13,3 +14,11 @@ export const load: PageServerLoad = async ({ fetch, url }) => {
         return { data: null };
     }
 };
+
+// for fetching tally data
+async function getTally(fetchFn: typeof fetch, province: string, city: string) {
+    const response = await fetchFn(
+        `http://165.245.190.93:8000/get-tally?province=${province}&city=${city}`,
+    );
+    parse(GetTallyResultSchema, JSON.parse(await response.text()));
+}
