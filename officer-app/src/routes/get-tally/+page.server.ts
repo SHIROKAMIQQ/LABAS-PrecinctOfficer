beaf3871b3f7f0b0b554334b946156ce8ead6072
Sidney Lance Fernando <scfernando@up.edu.ph>
2026-05-04 14:49:21 +0800

    refactor(`+page.server.ts`): use `.env` variables and `params`

--- officer-app/src/routes/get-tally/+page.server.ts
+++ officer-app/src/routes/get-tally/+page.server.ts
@@ -1,25 +1,28 @@
 import { parse } from 'valibot';
 import { GetTallyResultSchema } from '$lib/types';
 import { PUBLIC_API_IP, PUBLIC_API_PORT } from '$env/static/public';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch, url }) => {
     const province = url.searchParams.get('province');
     const city = url.searchParams.get('city');
 
-    try {
-        const data = await getTally(fetch, province || '', city || '');
-        return { data };
-    } catch (e) {
-        console.error(`getTally failed with error: ${e}`);
-        return { data: null };
-    }
+    const params = new URLSearchParams('');
+    if (province !== null) params.set('province', province);
+    if (city !== null) params.set('city', city);
+    
+    const data = await getTally(fetch, params);
+    return { data };
 };
 
 // for fetching tally data
-async function getTally(fetchFn: typeof fetch, province: string, city: string) {
+async function getTally(fetchFn: typeof fetch, params: URLSearchParams) {
     const response = await fetchFn(
-        `http://${PUBLIC_API_IP}:${PUBLIC_API_PORT}/get-tally?province=${province}&city=${city}`,
+        `http://${PUBLIC_API_IP}:${PUBLIC_API_PORT}/get-tally?${params}`,
     );
+    
+    if (!response.ok) throw error(response.status, response.statusText);
+
     parse(GetTallyResultSchema, JSON.parse(await response.text()));
 }
