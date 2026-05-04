4dc51e43bdbc51a8f544c165f3860683eb15b097
Sidney Lance Fernando <scfernando@up.edu.ph>
2026-05-04 12:18:03 +0800

    refactor: remove `tally/+page.ts`

--- officer-app/src/routes/tally/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = ({ url }) => {
-    return {
-        province: url.searchParams.get('province'),
-        city: url.searchParams.get('city'),
-    };
-};
